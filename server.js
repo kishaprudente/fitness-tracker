55aec44595a8897ee8cec9856578ca173d9a4bbb
Kisha Prudente <kisha.prudente@gmail.com>
2020-05-20 18:35:57 -0400

    added route for exercise and stats

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,13 +24,13 @@ app.get("/", (req, res) => {
 	res.send(index.html);
 });
 
-app.get("/api/workouts", async (req, res) => {
-	try {
-		const allWorkouts = await db.Workout.find({});
-		res.json(allWorkouts);
-	} catch (err) {
-		res.send(err);
-	}
+
+app.get("/exercise", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
 // Start the server
